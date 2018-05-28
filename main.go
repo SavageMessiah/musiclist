16834dbdbec8fb89439c16fe577531e89c1460bc
Ken Allen <cardboard42@gmail.com>
2018-05-28 02:28:33 -0400

    bad code but does the job

---
+++ main.go
@@ -0,0 +1,166 @@
+package main
+
+import (
+	"flag"
+	"github.com/BurntSushi/toml"
+	"github.com/djherbis/times"
+	"html/template"
+	"log"
+	"os"
+	"path/filepath"
+	"sort"
+	"time"
+)
+
+var t = template.Must(template.New("main").Parse(`
+<html>
+<head>
+  <title>HAHA I MADE MUSICS</title>
+</head>
+<body>
+{{range .}}
+<h1>{{.Title}}</h1>
+<p>{{.Desc}}</p>
+<ul>
+  {{range .Tracks}}
+    <li>
+    <div class="track">
+      <div class="head">
+        <a class="file" href="{{.File}}">{{.File}}</a>&nbsp;
+        <span class="made">({{.Made}})</span>
+      </div>
+      {{if .Title}}
+      <div class="title">{{.Title}}</div>
+      {{end}}
+      <audio controls>
+        <source src="{{.File}}" type="audio/mpeg"/>
+      </audio>
+      <p>{{.Desc}}</p>
+    </div>
+    </li>
+  {{end}}
+</ul>
+{{end}}
+</body>
+</html>
+`))
+
+var dir = flag.String("dir", ".", "The directory to read.")
+
+type Track struct {
+	File    string
+	Title   string
+	Desc    string
+	Made    string
+	madeRaw time.Time
+}
+
+type Tracks []Track
+
+func (t Tracks) Len() int           { return len(t) }
+func (t Tracks) Less(i, j int) bool { return t[i].madeRaw.After(t[j].madeRaw) }
+func (t Tracks) Swap(i, j int)      { t[i], t[j] = t[j], t[i] }
+
+type Section struct {
+	Title  string
+	Desc   string
+	Tracks Tracks
+}
+
+type TOMLTrack struct {
+	Title   string
+	Desc    string
+	Section string
+}
+
+type TOMLSections struct {
+	Section []*struct {
+		Short string
+		Title string
+		Desc  string
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	mp3files, err := filepath.Glob(filepath.Join(*dir, "*.mp3"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ts := TOMLSections{}
+	_, err = toml.DecodeFile(filepath.Join(*dir, "sections.toml"), &ts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sections := make(map[string]*Section)
+	for _, s := range ts.Section {
+		if s.Title == "" {
+			log.Fatal("Section with no title")
+		}
+		if s.Short == "" {
+			s.Short = s.Title
+		}
+		sections[s.Short] = &Section{
+			Title:  s.Title,
+			Desc:   s.Desc,
+			Tracks: []Track{},
+		}
+	}
+	for _, mp3file := range mp3files {
+		log.Println("Looking at mp3:", mp3file)
+		base := filepath.Base(mp3file)
+		clean := base[0 : len(base)-4] //remove .mp3
+		meta := filepath.Join(*dir, clean+".toml")
+
+		ft, err := times.Stat(mp3file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		tt := TOMLTrack{
+			Section: "Uncategorized",
+		}
+		if _, err := os.Stat(meta); err == nil {
+			log.Println("Found meta")
+			_, err := toml.DecodeFile(meta, &tt)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+
+		track := Track{
+			Title:   tt.Title,
+			Desc:    tt.Desc,
+			File:    base,
+			Made:    ft.BirthTime().Format("Jan 2, 2006"),
+			madeRaw: ft.BirthTime(),
+		}
+		section, ok := sections[tt.Section]
+		if !ok {
+			log.Fatalf("Missing section: %s", tt.Section)
+		}
+		log.Printf("Adding %s to %s", track.Title, tt.Section)
+		section.Tracks = append(section.Tracks, track)
+	}
+
+	sectionsList := []Section{}
+
+	for _, s := range ts.Section {
+		section := sections[s.Short]
+		sort.Stable(section.Tracks)
+		sectionsList = append(sectionsList, *section)
+	}
+
+	out, err := os.Create(filepath.Join(*dir, "index.html"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+
+	if err := t.Execute(out, sectionsList); err != nil {
+		log.Fatal(err)
+	}
+}
